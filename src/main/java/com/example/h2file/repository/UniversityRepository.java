da8042dc48b3ab5d890963ecc72d810ed4a255e4
camilo.barros <camilo.barros@globant.com>
2021-11-24 16:00:48 -0300

    one to one unidirectional

--- src/main/java/com/example/h2file/repository/UniversityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.h2file.repository;
-
-import com.example.h2file.model.onetomanyunidirectional.University;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UniversityRepository extends CrudRepository<University,Long> {
-}
