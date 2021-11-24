6678af8d772a953c6cbe1c159bf4343694adcfab
camilo.barros <camilo.barros@globant.com>
2021-11-24 15:46:52 -0300

    removed unused repositories

--- src/main/java/com/example/h2file/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.h2file.repository;
-
-import com.example.h2file.model.onetooneunidirectional.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-}
