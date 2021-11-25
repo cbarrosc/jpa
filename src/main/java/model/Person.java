8680b744659ff929d59db8f6d25978ead45b3127
camilo.barros <camilo.barros@globant.com>
2021-11-25 14:31:57 -0300

    many to many bidirectional

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,27 @@
+package model;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Person")
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Integer age;
+
+    public Person(String firstName, String lastName, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PERSON (first_name, last_name, age) values ('John', 'Doe', '20')
+INSERT INTO PERSON (first_name, last_name, age) values ('Tom', 'Smith', '32')
+INSERT INTO PERSON (first_name, last_name, age) values ('Jane', 'Smith', '30')
+INSERT INTO PERSON (first_name, last_name, age) values ('Cam', 'B', '30')
