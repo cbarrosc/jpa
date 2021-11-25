2773bc72a35ef8ae0b06aec7aadfc3adabdbdd5f
camilo.barros <camilo.barros@globant.com>
2021-11-25 14:40:01 -0300

    many to many bidirectional

--- src/main/java/com/example/h2file/H2FileApplication.java
+++ src/main/java/com/example/h2file/H2FileApplication.java
@@ -1,9 +1,11 @@
 package com.example.h2file;
 
 import lombok.extern.slf4j.Slf4j;
+import model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import repository.PersonRepository;
 
 @SpringBootApplication
 @Slf4j
@@ -12,6 +14,9 @@ public class H2FileApplication {
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(H2FileApplication.class, args);
+		PersonRepository personRepository = configurableApplicationContext.getBean(PersonRepository.class);
+		Person person = new Person("Chris", "Anderson",32);
+		personRepository.save(person);
 
 	}
 
