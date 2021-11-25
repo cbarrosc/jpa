cec96c4fb9b52e6759b5e99a2ec5aae89d37b9ae
camilo.barros <camilo.barros@globant.com>
2021-11-25 09:42:51 -0300

    clean branch

--- src/main/java/com/example/h2file/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.h2file.repository;
-
-import com.example.h2file.model.onetooneunidirectional.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepository extends CrudRepository<Address,Long> {
-}
--- src/main/java/com/example/h2file/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.h2file.repository;
-
-import com.example.h2file.model.onetoonebidirectional.Car;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends CrudRepository<Car,Long> {
-}
